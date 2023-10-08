638bfb9ece6fd18bb5bf3433894391b30dd68181
Al Rubio <alrubiom.web@gmail.com>
2023-10-08 12:01:13 +0200

    Archivos originales del proyecto

--- src/middleware/errors.js
+++ src/middleware/errors.js
@@ -1,11 +1,13 @@
+// Función para crear y manejar errores personalizados
 function error (message, code) {
-	let e = new Error(message);
+	let e = new Error(message); // Crea un nuevo objeto Error con el mensaje proporcionado
 
 	if(code) {
-		e.statusCode = code;
+		e.statusCode = code; // Si se proporciona un código, se asigna al objeto Error
 	}
 
-	return e;
+	return e; // Retorna el objeto Error
 }
 
+// Exporta la función para ser usada en otros archivos
 module.exports = error;
